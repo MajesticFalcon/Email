06474a47619806af13764c981031212ba3febf94
MajesticFalcon <Schylarutley@hotmail.com>
2014-08-29 15:53:09 -0500

    Adding plain text function

--- Email_Client.rb
+++ Email_Client.rb
@@ -9,40 +9,62 @@ class Email_Client
 
 attr_accessor :message_contents, :subject
 
-	def initialize(sender_name, receiver_name, sender_email, receiver_email)
-		@sender_name = sender_name
-		@receiver_name = receiver_name
-		@sender_email = sender_email
-		@receiver_email = receiver_email
-	end
+  def initialize(sender_name, receiver_name, sender_email, receiver_email)
+    @sender_name = sender_name
+    @receiver_name = receiver_name
+    @sender_email = sender_email
+    @receiver_email = receiver_email
+  end
 
 message_contents = ""
 subject = ""
 
-	def send_message
+def send_html
+
 message = <<MESSAGE
 From: #{@sender_name} <#{@sender_email}>
 To: #{@receiver_name} <#{@receiver_email}>
 MIME-Version: 1.0
 Content-type: text/html
 Subject: #{subject}
-		
+    
 #{message_contents}
-		
+    
 MESSAGE
-		Net::SMTP.start('smtp.ecarthage.com') do |smtp|
-		smtp.send_message message,
-		@sender_email, 
-		@receiver_email
-	end
-	
+
+      Net::SMTP.start('smtp.ecarthage.com') do |smtp|
+      smtp.send_message message,
+      @sender_email, 
+      @receiver_email
+      end
+  
 end
 
+  def send_email 
+message = <<MESSAGE
+From: #{@sender_name} <#{@sender_email}>
+To: #{@receiver_name} <#{@receiver_email}>
+MIME-Version: 1.0
+Content-type: text/plain
+Subject: #{subject}
 
-	def send_attached_message filename
-		filecontent = File.read(filename)
-		encodedcontent = [filecontent].pack("m")
-		marker = "END OF SECTION"
+#{message_contents}
+
+
+MESSAGE
+      Net::SMTP.start('smtp.ecarthage.com') do |smtp|
+      smtp.send_message message,
+      @sender_email, 
+      @receiver_email
+      end
+  end
+  
+  
+  def send_attached_message filename
+    filecontent = File.read(filename)
+    encodedcontent = [filecontent].pack("m")
+    marker = "END OF SECTION"
+    
 message = <<MESSAGE
 From: #{@sender_name} <#{@sender_email}> 
 To: #{@receiver_name} <#{@receiver_email}>
@@ -62,12 +84,11 @@ Content-Disposition: attachment; filename="#{filename}"
 #{encodedcontent}
 --#{marker}--
 MESSAGE
-		
-		Net::SMTP.start('smtp.ecarthage.com') do |smtp|
-		smtp.send_message message,
-		@sender_email, 
-		@receiver_email
-		end
-	end
+    
+    Net::SMTP.start('smtp.ecarthage.com') do |smtp|
+    smtp.send_message message,
+    @sender_email, 
+    @receiver_email
+    end
+  end
 end
-
